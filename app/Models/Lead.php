8aa8e2f1d955e20f235a24efb75e309bf94397bf
allison <a.oliveira@vertex.vertexdigital.co>
2025-03-21 17:31:38 -0300

    desenvolvendo a parte do lead no sistema

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-use App\Enums\{LeadStatusEnum, ServiceTypeEnum};
+use App\Enums\LeadStatusEnum;
+use App\Enums\ServiceTypeEnum;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lead extends Model
 {
@@ -27,8 +30,8 @@ class Lead extends Model
     ];
 
     protected $casts = [
-        'status'     => LeadStatusEnum::class,
-        'services'   => ServiceTypeEnum::class,
+        'status' => LeadStatusEnum::class,
+        'services' => ServiceTypeEnum::class,
         'created_at' => 'datetime:d-m-Y H:i:s',
         'updated_at' => 'datetime:d-m-Y H:i:s',
     ];
